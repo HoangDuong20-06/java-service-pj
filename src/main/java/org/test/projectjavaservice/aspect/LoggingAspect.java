5be74e810f7e9946d9aa7ccda7d4ade1aad771c7
admin <hoangduong2062006@gmail.com>
2026-06-15 12:53:08 +0700

    day3

--- src/main/java/org/test/projectjavaservice/aspect/LoggingAspect.java
+++ src/main/java/org/test/projectjavaservice/aspect/LoggingAspect.java
@@ -1,13 +1,17 @@
 package org.test.projectjavaservice.aspect;
 
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 import org.test.projectjavaservice.modal.dto.res.BookingResponse;
 
 @Aspect
 @Component
+@Slf4j
 public class LoggingAspect {
     @AfterReturning(
             pointcut = "execution(* org.test.projectjavaservice.service.BookingService.createBooking(..))",
@@ -27,5 +31,28 @@ public class LoggingAspect {
     )
     public void logBookingFailed(Exception exception) {
         System.err.printf("[AUDIT - FAILED] The request to book the field failed due to an error.: %s%n", exception.getMessage());
+    }
+    @Around("execution(* org.test.projectjavaservice.service..*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        try {
+            Object result = joinPoint.proceed();
+            long executionTime = System.currentTimeMillis() - start;
+            log.info(
+                    "[SUCCESS] Method={} Time={}ms",
+                    joinPoint.getSignature().getName(),
+                    executionTime
+            );
+            return result;
+        } catch (Exception e) {
+            long executionTime = System.currentTimeMillis() - start;
+            log.error(
+                    "[FAILED] Method={} Time={}ms Error={}",
+                    joinPoint.getSignature().getName(),
+                    executionTime,
+                    e.getMessage()
+            );
+            throw e;
+        }
     }
 }
--- src/main/java/org/test/projectjavaservice/controller/ManagerController.java
+++ src/main/java/org/test/projectjavaservice/controller/ManagerController.java
@@ -5,10 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.test.projectjavaservice.modal.dto.req.UpdateBookingStatusRequest;
 import org.test.projectjavaservice.modal.dto.res.ApiResponse;
 import org.test.projectjavaservice.modal.dto.res.BookingResponse;
 import org.test.projectjavaservice.service.BookingService;
+import org.test.projectjavaservice.service.ManagerSevice;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/manager")
@@ -16,6 +20,7 @@ import org.test.projectjavaservice.service.BookingService;
 @PreAuthorize("hasAnyRole('MANAGER','ADMIN')")
 public class ManagerController {
     private final BookingService bookingService;
+    private final ManagerSevice managerSevice;
     @PatchMapping("/{id}/status")
     public ResponseEntity<ApiResponse<BookingResponse>> updateStatus(@PathVariable Long id, @Valid @RequestBody UpdateBookingStatusRequest request) {
         BookingResponse data = bookingService.updateStatus(id, request);
@@ -24,6 +29,17 @@ public class ManagerController {
                 .message("Cập nhật trạng thái đơn đặt sân thành công")
                 .data(data)
                 .build();
+        return ResponseEntity.ok(response);
+    }
+    @PostMapping("/courts/{courtId}/images")
+    public ResponseEntity<ApiResponse<List<String>>> uploadCourtImages(@PathVariable Long courtId, @RequestParam("files") List<MultipartFile> files) {
+        List<String> urls = managerSevice.uploadCourtImages(courtId, files);
+        ApiResponse<List<String>> response = ApiResponse.<List<String>>builder()
+                .success(true)
+                .message("Upload ảnh thành công")
+                .data(urls)
+                .build();
+
         return ResponseEntity.ok(response);
     }
 }
